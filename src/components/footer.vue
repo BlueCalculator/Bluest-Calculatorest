6a34f5ea4a84c8c1b6a8e9677bb6ac54be5baf32
Blue Calculator <85308130+BlueCalculator@users.noreply.github.com>
2026-04-07 10:15:58 -0400

    initial commit

---
+++ src/components/footer.vue
@@ -0,0 +1,185 @@
+<template>
+    <footer>
+      <p>Feeter</p>
+      <p>Touch 'em</p>
+      <div class="toes"> 
+        <div class="left toeBox">
+          <div class="pinkey"></div>
+          <div class="forth"></div>          
+          <div class="thrid"></div>          
+          <div class="second"></div> 
+          <div class="big"></div>
+        </div>
+        <div class="right toeBox">
+          <a class="toeLink" href="https://github.com/BlueCalculator" target="_blank" rel="noopener noreferrer">
+            <div class="big"></div>
+          </a>
+          <div class="second"></div>          
+          <div class="thrid"></div>          
+          <div class="forth"></div>          
+          <div class="pinkey"></div>
+        </div>        
+      </div>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        width: 100vw;
+        height: 180px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        border-top: 1px solid var(--solidLine);
+    }    
+    .toes{
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      gap: -230px;
+    }
+    .toeBox{
+      display: flex;
+      flex-direction: row;
+      align-items: baseline;
+      gap: 2px;
+    }
+    .toeLink{
+      text-decoration: none;
+      color: black;
+    }
+    .right::after{
+      content: '';
+      width: 230px;
+      height: 0;
+      border-bottom: 60px solid peachpuff;
+      border-left: 50px solid transparent;
+      border-right: 180px solid transparent;
+      border-top: 0px solid transparent;
+      width: 100px;
+      bottom: 0;
+      position: absolute;
+    }
+    .left{
+      position: relative;
+      margin-right: 10px;
+    }
+    .right{
+      position: relative;
+    }
+    .left::after{
+      position: absolute;
+      content: '';
+      width: 230px;
+      height: 0;
+      border-bottom: 60px solid peachpuff;
+      border-left: 180px solid transparent;
+      border-right: 50px solid transparent;
+      border-top: 0px solid transparent;
+      width: 100px;
+      bottom: 0;
+    }
+    .big{
+      position: relative;
+      background-color: peachpuff;
+      width: 60px;
+      height: 120px;
+      border-radius: 25px 25px 0 0;
+      box-shadow: 0 -4px 10px 2px rgba(0,0,0,0.05);
+    }
+    .big::after{
+      content: '';
+      position: absolute;
+      background-color: rgb(255, 233, 213);
+      display: block;
+      width: 50px;
+      height: 40px;
+      border-radius: 28px 28px 40px 35px;
+      transform: translate(10%, 20%);
+      transition: .5s;
+      box-shadow: inset 0px -5px 4px 0px rgb(252, 219, 191);
+    }
+    .big:hover::after {
+      content: "Github";
+      transform: scale(1.1) translate(10%, 10%);
+    }
+    .second{
+      position: relative;
+      background-color: peachpuff;
+      width: 40px;
+      height: 130px;
+      border-radius: 15px 15px 0 0;
+      box-shadow: 0 -4px 10px 2px rgba(0,0,0,0.05);
+    }
+    .second::after{
+      content: '';
+      background-color: rgb(255, 233, 213);
+      display: block;
+      width: 33px;
+      height: 30px;
+      border-radius: 28px 28px 40px 35px;
+      transform: translate(10%, 20%);
+      box-shadow: inset 0px -2px 4px 0px rgb(252, 219, 191);
+    }
+    .thrid{
+      position: relative;
+      background-color: peachpuff;
+      width: 40px;
+      height: 105px;
+      border-radius: 15px 15px 0 0;
+      box-shadow: 0 -4px 10px 2px rgba(0,0,0,0.05);
+    }
+    .thrid::after{
+      content: '';
+      background-color: rgb(255, 233, 213);
+      display: block;
+      width: 33px;
+      height: 30px;
+      border-radius: 28px 28px 40px 35px;
+      transform: translate(10%, 20%);
+      box-shadow: inset 0px -2px 4px 0px rgb(252, 219, 191);
+    }
+    .forth{
+      position: relative;
+      background-color: peachpuff;
+      width: 40px;
+      height: 75px;
+      border-radius: 15px 15px 0 0;
+      box-shadow: 0 -4px 10px 2px rgba(0,0,0,0.05);
+    }
+    .forth::after{
+      content: '';
+      background-color: rgb(255, 233, 213);
+      display: block;
+      width: 33px;
+      height: 30px;
+      border-radius: 28px 28px 30px 35px;
+      transform: translate(10%, 20%);
+      box-shadow: inset 0px -2px 4px 0px rgb(252, 219, 191);
+    }
+    .pinkey{
+      position: relative;
+      background-color: peachpuff;
+      width: 40px;
+      height: 45px;
+      border-radius: 15px 15px 0 0;
+      box-shadow: 0 -4px 10px 2px rgba(0,0,0,0.05);
+    }
+    .pinkey::after{
+      content: '';
+      background-color: rgb(255, 233, 213);
+      display: block;
+      width: 33px;
+      height: 22px;
+      border-radius: 38px 38px 40px 35px;
+      transform: translate(10%, 20%);
+      box-shadow: inset 0px -2px 4px 0px rgb(252, 219, 191);
+    }
+
+    @media (max-width: 480px) {
+      .left{
+        display: none;
+      }
+    }
+</style>
---
+++ src/components/topNav.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="topNav">
+        <div class="rightTopNav navItem">
+            <a href="/"><img src="/imgs/blueKing.png" alt="Calc Logo"></a>
+        </div>
+        <div class="purse">
+            <p class="tagPurse">Purse:</p>
+            <div id="purseAmount"><p>{{ purseAmount || 'Loading...' }} Loonies</p></div>
+        </div>
+        <div class="leftTopNav navItem">
+            <a href="market/">Market</a>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .navItem{
+        width: 100px;
+    }
+
+    .topNav{
+        width: 100vw;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px;
+        border-bottom: 1px solid var(--solidLine);
+    }
+    .tagPurse{
+        font-size: .8rem;
+    }
+    .purse{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 8rem;
+    }
+    #purseAmount {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid var(--solidLine);
+        border-radius: 10px;
+        padding: 5px;
+    }
+    .rightTopNav > a > img {
+        width: auto;
+        height: 90px;        
+        margin-left: 10px;
+    }
+
+    .leftTopNav {
+        display: flex;
+        gap: 10px;
+        margin-right: 10px;
+    }
+
+    .leftTopNav a {
+        transition: .2s;
+        text-decoration: none;
+        color: var(--solidLine);
+    }
+
+    .leftTopNav > a:hover {
+        animation: bigSmall .6s forwards;
+    }
+
+    @keyframes bigSmall {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.15);
+        }
+        100% {
+            transform: scale(1.1);
+        }
+    }
+
+</style>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+
+const purseAmount = ref('')
+
+onMounted(() => {
+  const pathSegments = window.location.pathname.split('/')
+  const idFromUrl = pathSegments[pathSegments.length - 1]
+  const stored = localStorage.getItem('purseAmount')
+  
+  if (stored) {
+    purseAmount.value = stored
+  } else {
+    purseAmount.value = idFromUrl || '111'
+    localStorage.setItem('purseAmount', purseAmount.value)
+    console.log('Set purseAmount:', purseAmount.value)
+  }
+
+  if (!window.localStoragePatched) {
+    const originalSetItem = localStorage.setItem
+    localStorage.setItem = function(key, value) {
+      const event = new CustomEvent('local-storage', { detail: { key, value } })
+      window.dispatchEvent(event)
+      originalSetItem.apply(this, arguments)
+    }
+    window.localStoragePatched = true
+  }
+
+  window.addEventListener('local-storage', (e) => {
+    if (e.detail.key === 'purseAmount') {
+      purseAmount.value = e.detail.value
+    }
+  })
+
+  // Listen to cross-tab updates natively
+  window.addEventListener('storage', (e) => {
+    if (e.key === 'purseAmount' && e.newValue) {
+      purseAmount.value = e.newValue
+    }
+  })
+})
+
+// Auto-sync changes
+watch(purseAmount, (newAmount) => {
+  if (newAmount) localStorage.setItem('purseAmount', newAmount)
+})
+</script>
