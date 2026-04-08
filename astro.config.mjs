ee9446fffe4adb61f22c30d3801bcb44a369883e
Blue Calculator <85308130+BlueCalculator@users.noreply.github.com>
2026-04-07 20:56:19 -0400

    Fixed Github pages in the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://BlueCalculator.github.io',
-  base: '/Bluest-Calculatorest',
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: cloudflare()
 });
